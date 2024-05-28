e0399d4f1c0f464355ab54ba22f8ca229bebcb14
zrjake <zjake128@gmail.com>
2024-05-28 18:43:40 -0500

    Outlined script code

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Working!")
+// This is a simple rock paper scissors game
+// that runs entirely in the console, initiated
+// by the function playGame and taking input
+// from prompt.
+
+// Runs playRound 5 times, tracking score
+// and declaring a winner.
+function playGame() {
+
+} 
+
+// Determines the winner given players' choices.
+function playRound(humanChoice, computerChoice) {
+
+}
+
+// Prompts user for a choice, returning lowercase input.
+function getHumanChoice() {
+
+}
+
+// Generates rock/paper/scissors randomly.
+function getComputerChoice() {
+
+}
