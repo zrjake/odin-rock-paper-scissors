332c67ae96c5b55b3729fd00489765bb905e02a1
zrjake <zjake128@gmail.com>
2024-05-28 19:04:46 -0500

    Add an overall game function which plays five rounds and determines a winner

--- script.js
+++ script.js
@@ -6,34 +6,45 @@
 // Runs playRound 5 times, tracking score
 // and declaring a winner.
 function playGame() {
-
-} 
-
-let humanScore = 0;
-let computerScore = 0;
-const humanChoice = getHumanChoice();
-const computerChoice = getComputerChoice();
-playRound(humanChoice, computerChoice);
-// Determines the winner given players' choices.
-function playRound(humanChoice, computerChoice) {
-    // Three possibilites: tie, comp wins, human wins.
-    // H beats C if: H is rock, C is scissors
-    //               H is paper, C is rock
-    //               H is scissors, C is paper
-    // Everything that isn't a tie or one of these
-    // is a victory for C.
-    if (humanChoice == computerChoice) {
-        console.log("It's a tie! No points awarded.");
-    } else if ((humanChoice == "rock" && computerChoice == "scissors")
-            || (humanChoice == "paper" && computerChoice == "rock")
-            || (humanChoice == "scissors" && computerChoice == "paper")) {
-        console.log("You win! +1 point");
-        humanScore++;
-    } else {
-        console.log("I win!");
-        computerScore++;
+    // Determines the winner given players' choices.
+    function playRound(humanChoice, computerChoice) {
+        // Three possibilites: tie, comp wins, human wins.
+        // H beats C if: H is rock, C is scissors
+        //               H is paper, C is rock
+        //               H is scissors, C is paper
+        // Everything that isn't a tie or one of these
+        // is a victory for C.
+        if (humanChoice == computerChoice) {
+            console.log("It's a tie! No points awarded.");
+        } else if ((humanChoice == "rock" && computerChoice == "scissors")
+                || (humanChoice == "paper" && computerChoice == "rock")
+                || (humanChoice == "scissors" && computerChoice == "paper")) {
+            console.log("You win! +1 point");
+            humanScore++;
+        } else {
+            console.log("I win!");
+            computerScore++;
+        }
     }
-}
+
+    // Run five rounds of the game.
+    let humanScore = 0;
+    let computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = getComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+
+    console.log(`Final score: ${humanScore} for you, ${computerScore} for me.`);
+    if (humanScore > computerScore) {
+        console.log("You win!");
+    } else if (computerScore > humanScore) {
+        console.log("I win!");
+    } else {
+        console.log("It's a tie!");
+    }
+} 
 
 // Prompts user for a choice, returning lowercase input.
 function getHumanChoice() {
